d670877e0d1ef27cf18960e4bf8146a41e5816bb
Eno <eno.imac31@gmail.com>
2023-03-05 16:07:21 +0100

    TP2 - Exos 1 à 3 finis, Exo 4 en cours, Exo 5 pas commencé

--- TP2/exo1.cpp
+++ TP2/exo1.cpp
@@ -6,19 +6,26 @@
 MainWindow* w = nullptr;
 
 void selectionSort(Array& toSort){
-	// selectionSort
 //    t ⇐ tableau de nombre aléatoire
 //    Pour chaque indice i de t faire
 //    chercher le minimum à partir de i
 //    inverser le minimum et la case courante
 //    fin Pour
+    // On parcours le tableau i fois au total, en avançant d'une case à chaque fois
     for(uint i=0; i<toSort.size(); i++){
+        // On utilise un compteur pour se souvenir de l'indice du tableau le plus petit
         int comp = i;
+
+        // On parcours le tableau pour trouver la valeur la plus petite
         for(uint j=i+1; j<toSort.size(); j++){
+            // Si on trouve une valeur plus petite
             if(toSort[j] < toSort[comp]){
+                // Notre compteur retient son emplacement dans le tableau
                 comp = j;
             }
         }
+
+        // On inverse les valeurs (celle de l'indice auquel on est avec la plus petite) à l'aide d'une variable temporaire
         int tmp = toSort[i];
         toSort[i] = toSort[comp];
         toSort[comp] = tmp;
--- TP2/exo4.cpp
+++ TP2/exo4.cpp
@@ -17,28 +17,35 @@ void recursivQuickSort(Array& toSort, int size)
 
 	// stop statement = condition + return (return stop the function even if it does not return anything)
 	
+    // Création du pivot (qui vaut la moitié de la taille du tableau)
     int pivot = size/2;
 
+    // Création des deux tableaux qui contiendrons les "moitié" de tableau
 	Array& lowerArray = w->newArray(size);
 	Array& greaterArray= w->newArray(size);
 	int lowerSize = 0, greaterSize = 0; // effectives sizes
 
-	// split
+    // split : On va séparer le tableau en deux
+    // On parcours la première moitié du tableau
     for(int i=0; i<= pivot-1; i++){
+        // On remplit un des tableaux crées avec le grand tableau
         lowerArray[i] = toSort[i];
     }
     int compteur = 0;
+    // On parcours la seconde moitié du tableau
     for(int i=pivot; i<= size-1; i++){
+        // On remplit l'autre tableau crée avec le grand tableau
         greaterArray[compteur] = toSort[i];
         compteur++;
     }
 	
-	// recursiv sort of lowerArray and greaterArray
+    // recursiv sort of lowerArray and greaterArray : On rappelle la fonction tant que la taille des tableaux est plus grande que 1
     if(size>1){
         return(recursivQuickSort(lowerArray, pivot));
         return(recursivQuickSort(greaterArray, pivot));
     }
-	// merge
+
+    // merge : on va fusionner les tableaux dans le bon ordre
 }
 
 void quickSort(Array& toSort){
