4b62cbdaac5958786ddf2a8ae3a2afb75c2dc8b5
EnoTurtle <eno.imac31@gmail.com>
2023-05-31 11:45:55 +0200

    TP6 terminé !

--- TP6/exo1.cpp
+++ TP6/exo1.cpp
@@ -12,6 +12,19 @@ void Graph::buildFromAdjenciesMatrix(int **adjacencies, int nodeCount)
 	  * this->appendNewNode
 	  * this->nodes[i]->appendNewEdge
 	  */
+
+    for(int i=0; i<nodeCount; i++){
+        this->appendNewNode(new GraphNode(i));
+    }
+
+    // On a besoin de deux boucles for car on travaille avec une matrice qui est donc en deux dimensions
+    for (int i = 0; i < nodeCount; i++) {
+        for (int j = 0; j < nodeCount; j++) {
+            if (adjacencies[i][j] != 0) {
+                this->nodes[i]->appendNewEdge(this->nodes[j], adjacencies[i][j]);
+            }
+        }
+    }
 }
 
 void Graph::deepTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, bool visited[])
@@ -20,6 +33,14 @@ void Graph::deepTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, boo
 	  * Fill nodes array by travelling graph starting from first and using recursivity
 	  */
 
+    // Application de l'algorithme du parcours en profondeur
+    visited[first->value] = true;
+    for (Edge *e = first->edges; e != nullptr; e = e->next) {
+        if (!visited[e->destination->value]) {
+            // On ré-appelle la fonction
+            deepTravel(e->destination, nodes, nodesSize, visited);
+        }
+    }
 }
 
 void Graph::wideTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, bool visited[])
@@ -33,6 +54,21 @@ void Graph::wideTravel(GraphNode *first, GraphNode *nodes[], int &nodesSize, boo
 	 */
 	std::queue<GraphNode*> nodeQueue;
 	nodeQueue.push(first);
+
+    // Application de l'algorithme du parcours en largeur
+    while (!nodeQueue.empty()) {
+        GraphNode* current = nodeQueue.front();
+        nodeQueue.pop();
+
+        nodes[nodesSize++] = current;
+        visited[current->value] = true;
+
+        for (Edge *e = current->edges; e != nullptr; e = e->next) {
+             if (!visited[e->destination->value]) {
+                nodeQueue.push(e->destination);
+            }
+        }
+    }
 }
 
 bool Graph::detectCycle(GraphNode *first, bool visited[])
@@ -41,7 +77,22 @@ bool Graph::detectCycle(GraphNode *first, bool visited[])
 	  Detect if there is cycle when starting from first
 	  (the first may not be in the cycle)
 	  Think about what's happen when you get an already visited node
-	**/
+    **/
+    // On commence par mettre le noeud actuel comme déjà visité
+    visited[first->value] = true;
+
+    for (Edge *e = first->edges; e != nullptr; e = e->next) {
+        // Cas où le noeud à côté n'est pas visité, sinon le noeud a déjà été visité
+        if (!visited[e->destination->value]) {
+            if (detectCycle(e->destination, visited)) {
+                return true;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    visited[first->value] = false;
     return false;
 }
 
